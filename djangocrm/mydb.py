e63edaef1382081e8d485bca5eb98f3280be5a22
nahidn4p <nahidn4p@gmail.com>
2025-07-27 13:58:41 +0600

    initial commit

---
+++ djangocrm/mydb.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+
+dataBase = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="pass123",
+)
+
+
+#preparing a cursor object
+cursorObject = dataBase.cursor()
+
+#creating a database
+cursorObject.execute("CREATE DATABASE IF NOT EXISTS djangocrm_db")  
+print("Database created successfully!")
